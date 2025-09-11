110e0f704bc4252778eda3cded85d236fe8673fd
Abhijit Desktop <abhijitnumber1@gmail.com>
2025-09-12 01:14:43 +0530

    implemented react simple md editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,12 +1,15 @@
+"use client";
+
 import { Button, TextArea, TextField } from "@radix-ui/themes";
 import React from "react";
-
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 const NewIssue = () => {
 	return (
 		<div className="max-w-lg p-4 space-y-3">
 			<TextField.Root placeholder="Search the docs…"></TextField.Root>
 
-			<TextArea placeholder="Reply to comment…" />
+			<SimpleMDE placeholder="Reply to comment…" />
 			<Button>Create New Issue</Button>
 		</div>
 	);
