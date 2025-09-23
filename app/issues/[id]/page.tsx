4206943ba926c6bf1c2b2d15e28441baddc3a7c6
Abhijit Desktop <abhijitnumber1@gmail.com>
2025-09-24 00:42:49 +0530

    Edit icon created

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,30 +1,42 @@
 import StatusBadge from "@/app/components/StatusBadge";
 import prisma from "@/prisma/client";
-import { Box, Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import React from "react";
 import ReactMarkdown from "react-markdown";
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 const IndividualIssues = async ({ params }: { params: { id: string } }) => {
+	// Await the params object before using its properties
+	const { id } = await params;
 	const issue = await prisma.issue.findUnique({
 		where: {
-			id: parseInt(params.id),
+			id: parseInt(id),
 		},
 	});
 	if (!issue) {
 		notFound(); // 👈 Redirects to the 404 page
 	}
 	return (
-		<Box>
-			<Heading>{issue?.title}</Heading>
-			<Flex className="space-x-3" my="2">
-				<StatusBadge status={issue.status}></StatusBadge>
-				<Text>{issue.createdAt.toDateString()}</Text>
-			</Flex>
-			<Card className="prose">
-				<ReactMarkdown>{issue.description}</ReactMarkdown>
-			</Card>
-		</Box>
+		<Grid columns={{ initial: "1", md: "2" }} gap="5">
+			<Box>
+				<Heading>{issue?.title}</Heading>
+				<Flex className="space-x-3" my="2">
+					<StatusBadge status={issue.status}></StatusBadge>
+					<Text>{issue.createdAt.toDateString()}</Text>
+				</Flex>
+				<Card className="prose">
+					<ReactMarkdown>{issue.description}</ReactMarkdown>
+				</Card>
+			</Box>
+			<Box>
+				<Button>
+					<Pencil2Icon />
+					<Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
+				</Button>
+			</Box>
+		</Grid>
 	);
 };
 
